7fa939589c03599792c6666689bf51dd4d77d18c
zyong.dang <dduong@gmail.com>
2021-12-18 23:53:09 +0300

    inference implemented

---
+++ tester/test.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+
+import torch
+import torchaudio
+
+from featurizer import MelSpectrogram, MelSpectrogramConfig
+from models import Generator
+
+
+def test(
+        model_path: str,
+        wav_dir: str,    # directory of test files
+        mel_config: MelSpectrogramConfig = MelSpectrogramConfig(),
+        test_save_dir: str = None
+):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    gen = Generator(80, 512)
+    gen.load_state_dict(torch.load(model_path, device))
+    gen.eval()
+
+    featurizer = MelSpectrogram(mel_config)
+
+    wavs = []
+    wav_lengths = []
+    mels = []
+
+    reconstructed_wavs = []
+    reconstructed_mels = []
+    wav_files = os.listdir(wav_dir)
+    for wav_file in wav_files:
+        if wav_file.find('.wav') == -1:
+            continue
+        wavs.append(torchaudio.load(wav_dir + wav_file)[0])
+        wav_lengths.append(len(wavs[-1]))
+        mels.append(featurizer(wavs[-1]))
+        with torch.no_grad():
+            reconstructed_wavs.append(gen(mels[-1]))
+        reconstructed_mels.append(featurizer(reconstructed_wavs[-1]).squeeze(0).detach().cpu().numpy())
+        if test_save_dir is not None:
+            if not os.path.exists(test_save_dir):
+                os.mkdir(test_save_dir)
+            torchaudio.save(test_save_dir + wav_file, reconstructed_wavs[-1], sample_rate=mel_config.sr)
+    return (reconstructed_wavs, reconstructed_mels), mels
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "-m",
+        "--model",
+        default=None,
+        type=str,
+        help="generator model path (default: None)",
+    )
+    args.add_argument(
+        "-d",
+        "--data",
+        default=None,
+        type=str,
+        help="path to data to test (default: None)",
+    )
+    args.add_argument(
+        "-o",
+        "--out",
+        default=None,
+        type=str,
+        help="dir to save reconstructed wavs at (default: None)",
+    )
+
+    arg_dict = args.parse_args().__dict__
+    model_path = arg_dict['model']
+    wav_dir = arg_dict['data']
+    test_save_dir = arg_dict['out']
+    # config = ConfigParser.from_args(args, options)
+    # main(config)
+    # transcript = [
+    #     "A defibrillator is a device that gives a high energy electric shock to the heart of someone who is in cardiac arrest",
+    #     "Massachusetts Institute of Technology may be best known for its math, science and engineering education",
+    #     "Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space"
+    # ]
+    # model_path = '../test_data/gen.pth'
+    # print(os.listdir('../test_data/'))
+    test(model_path, wav_dir, test_save_dir=test_save_dir)
